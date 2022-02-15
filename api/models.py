7ae770f2721943f40d3f99d41bb01df82a54fed3
Jens Hovem Leonhardsen <jenshle@stud.ntnu.no>
2022-02-15 12:36:35 +0100

    Testing geom field in db

--- api/models.py
+++ api/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.gis.db import models
 
 class Developer(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
+
+class CurrentPosition(models.Model):
+    test = models.PositiveIntegerField(blank=True)
+    geom = models.GeometryField(blank=True)
         
