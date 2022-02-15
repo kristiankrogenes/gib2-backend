ada5989196ab37a315add3de4bc11d8c535093e1
Sigurd Bakkerud <sbakkerud1@gmail.com>
2022-02-15 13:43:56 +0100

    Make geometry field in models

--- api/models.py
+++ api/models.py
@@ -3,11 +3,12 @@ from django.contrib.gis.db import models
 
 class Developer(models.Model):
     name = models.CharField(max_length=30)
+    geom = models.PointField(blank=True, default="POINT(0.0 0.0)")
 
     def __str__(self):
         return self.name
 
 class CurrentPosition(models.Model):
     test = models.PositiveIntegerField(blank=True)
-    geom = models.GeometryField(blank=True)
+    geom = models.PointField(blank=True, default="POINT(0.0 0.0)")
         
